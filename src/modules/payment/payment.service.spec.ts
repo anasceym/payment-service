99791f12e15db7dad0b08a6f979614567d345788
anasceym <anasfirdauss@gmail.com>
2019-08-04 14:20:32 +0800

    Payment service provider

---
+++ src/modules/payment/payment.service.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { Order, Payment } from './payment.service'
+
+describe('Payment', () => {
+  let provider: Payment;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [Payment],
+    }).compile();
+
+    provider = module.get<Payment>(Payment);
+  });
+
+  it('should be defined', () => {
+    expect(provider).toBeDefined();
+  });
+
+  describe('Make a payment', () => {
+    it('resolve status "CONFIRMED" when payments successfully made', async () => {
+      const fakeOrder: Order = {
+        id: 'id-1234',
+        price: 1000
+      }
+
+      const result = await provider.make(fakeOrder)
+
+      expect(result.status).toMatch(/CONFIRMED|DECLINED/)
+      expect(result.order.id).toBe(fakeOrder.id)
+    })
+  })
+});
---
+++ src/modules/payment/payment.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common'
+
+export enum PAYMENT_STATUS {
+  DECLINED = 'DECLINED',
+  CONFIRMED = 'CONFIRMED'
+}
+
+export interface Order {
+  id: string
+  price: number
+}
+
+@Injectable()
+export class Payment {
+  async make (order: Order): Promise<{
+    order: Order,
+    status: PAYMENT_STATUS
+  }> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        return resolve({
+          order,
+          status: randomPaymentResult(PAYMENT_STATUS)
+        })
+      }, 3000)
+    })
+  }
+}
+
+function randomPaymentResult<T>(anEnum: T): T[keyof T] {
+  const enumValues = Object.keys(anEnum) as unknown as Array<T[keyof T]>
+
+  const randomIndex = Math.floor(Math.random() * enumValues.length)
+  const randomEnumValue = enumValues[randomIndex]
+  return randomEnumValue;
+}
