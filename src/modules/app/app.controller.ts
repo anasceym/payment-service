74e7b24a47fb4b11b5812307dbe903f65c70480a
anasceym <anasfirdauss@gmail.com>
2019-08-05 09:59:38 +0800

    Added tags for swag

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get } from '@nestjs/common'
-import { ApiOkResponse } from '@nestjs/swagger'
+import { ApiOkResponse, ApiOperation, ApiUseTags } from '@nestjs/swagger'
 
 import { AppService } from './app.service'
 
 @Controller()
+@ApiUseTags('Utility')
 export class AppController {
   constructor (private readonly appService: AppService) {}
 
-  @Get()
-  @ApiOkResponse({})
+  @Get('health')
+  @ApiOperation({
+    title: 'Service health check',
+    description: 'To check the healthiness of the service'
+  })
+  @ApiOkResponse({
+    description: 'Service healthy'
+  })
   healthCheck (): {
     status: string
   } {
