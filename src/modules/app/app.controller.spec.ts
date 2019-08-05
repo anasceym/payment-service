39c5e9587a889a82a8379c050dcd3512d1f0b53a
anasceym <anasfirdauss@gmail.com>
2019-08-05 09:54:26 +0800

    Added proper healthcheck

--- src/modules/app/app.controller.spec.ts
+++ src/modules/app/app.controller.spec.ts
@@ -15,9 +15,11 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController)
   })
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+  describe('healthCheck', () => {
+    it('should return status ok', () => {
+      expect(appController.healthCheck()).toEqual({
+        status: 'OK'
+      })
     })
   })
 })
--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common'
+import { ApiOkResponse } from '@nestjs/swagger'
 
 import { AppService } from './app.service'
 
@@ -7,7 +8,12 @@ export class AppController {
   constructor (private readonly appService: AppService) {}
 
   @Get()
-  getHello (): string {
-    return this.appService.getHello()
+  @ApiOkResponse({})
+  healthCheck (): {
+    status: string
+  } {
+    return {
+      status: 'OK'
+    }
   }
 }
